0943a5392c3283973c79d4dd91ae49a69ec7fd43
Prashant Sah <prashantsah567@gmail.com>
2023-02-13 16:58:11 -0500

    unncessary react files removed

---
+++ src/components/Create-cv.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react"
+
+class CreateCV extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <h1>This is the create - cv section</h1>
+        );
+    }
+}
+
+export default CreateCV;
---
+++ src/components/preview-cv.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class PreviewCV extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <h1>This is previewCV section</h1>
+        )
+    }
+}
+
+export default PreviewCV;
