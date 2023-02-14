0d22b0efd8663552d9fb643dbeda3f1fa9efc8cb
Prashant Sah <prashantsah567@gmail.com>
2023-02-13 19:31:46 -0500

    education and experience section added

--- src/components/Create-cv.js
+++ src/components/Create-cv.js
@@ -8,8 +8,34 @@ class CreateCV extends Component{
     render(){
         return(
             <div className="createCV">
-                <h1 className="header">Create - CV section</h1>
-                <hr/>
+                <form>
+                    <h3 className="header">Please fill out these following informations</h3>
+                    <div className="intro">
+                        <input placeholder="Name"/>
+                        <input placeholder="email" />
+                        <input placeholder="contact" />
+                        <input placeholder="location" />
+                    </div>
+                    <hr/>
+                    <div>
+                        <textarea placeholder="A short bio" required></textarea>
+                    </div>
+                    <div>
+                        <h3>Education Section</h3>
+                        <input placeholder="School or college name" className="Edu-Input"/><br/>
+                        <input placeholder="Major" className="Edu-Input"/><br/>
+                        <input placeholder="Start and end date" className="Edu-Input"/><br/>
+                        <input placeholder="GPA" className="Edu-Input"/><br/>
+                    </div>
+                    <div>
+                        <h3>Experience Section</h3>
+                        <input placeholder="Job title" className="Edu-Input"/><br/>
+                        <input placeholder="Company Name" className="Edu-Input"/><br/>
+                        <input placeholder="Company location" className="Edu-Input"/><br/>
+                        <input placeholder="start and end date" className="Edu-Input"/><br/>
+                        <textarea placeholder="description" className="Edu-Input"/><br/>
+                    </div>
+                </form>
             </div>
         );
     }
