1a076f3989e404ae5b37e6686cc24c5e889ac202
Prashant Sah <prashantsah567@gmail.com>
2023-02-14 08:57:48 -0500

    buttons added

--- src/components/Create-cv.js
+++ src/components/Create-cv.js
@@ -18,23 +18,35 @@ class CreateCV extends Component{
                     </div>
                     <hr/>
                     <div>
-                        <textarea placeholder="A short bio" required></textarea>
+                        <textarea className="bio"placeholder="A short bio" required></textarea>
                     </div>
-                    <div>
-                        <h3>Education Section</h3>
-                        <input placeholder="School or college name" className="Edu-Input"/><br/>
-                        <input placeholder="Major" className="Edu-Input"/><br/>
-                        <input placeholder="Start and end date" className="Edu-Input"/><br/>
-                        <input placeholder="GPA" className="Edu-Input"/><br/>
+                    <div className="mainContainer">
+                        <div className="mainSection1">
+                            <h3>Skills:</h3>
+                            <input className="skillInput" />
+                            <button>Add</button>
+                        </div>
+                        <div className="mainSection2">
+                            <div>
+                                <h3>Education Section</h3>
+                                <input placeholder="School or college name" className="Edu-Input"/><br/>
+                                <input placeholder="Major" className="Edu-Input"/><br/>
+                                <input placeholder="Start and end date" className="Edu-Input"/><br/>
+                                <input placeholder="GPA" className="Edu-Input"/><br/>
+                                <button>Add</button><button>Delete</button>
+                            </div>
+                            <div>
+                                <h3>Experience Section</h3>
+                                <input placeholder="Job title" className="Edu-Input"/><br/>
+                                <input placeholder="Company Name" className="Edu-Input"/><br/>
+                                <input placeholder="Company location" className="Edu-Input"/><br/>
+                                <input placeholder="start and end date" className="Edu-Input"/><br/>
+                                <textarea placeholder="description" className="Edu-Input"/><br/>
+                                <button>Add</button><button>Delete</button>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Experience Section</h3>
-                        <input placeholder="Job title" className="Edu-Input"/><br/>
-                        <input placeholder="Company Name" className="Edu-Input"/><br/>
-                        <input placeholder="Company location" className="Edu-Input"/><br/>
-                        <input placeholder="start and end date" className="Edu-Input"/><br/>
-                        <textarea placeholder="description" className="Edu-Input"/><br/>
-                    </div>
+                    
                 </form>
             </div>
         );
